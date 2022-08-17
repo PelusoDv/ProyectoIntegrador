fb339f7f95179dccdd7d72ead36e6dc45a27a3cf
PelusoDv <nico.vithe15@gmail.com>
2022-08-17 06:09:22 -0300

    creados y añadidos al modulo los nuevos componentes para comenzar a trabajar

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
