66da3a850ecc45a38ade8e206bbd0678ccedbf3f
PelusoDv <nico.vithe15@gmail.com>
2022-08-18 00:11:10 -0300

    añadida una nueva seccion de la pagina con info sobre mi

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { InfoPersonalComponent } from './components/info-personal/info-personal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    InfoPersonalComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio/src/app/components/info-personal/info-personal.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoPersonalComponent } from './info-personal.component';
+
+describe('InfoPersonalComponent', () => {
+  let component: InfoPersonalComponent;
+  let fixture: ComponentFixture<InfoPersonalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InfoPersonalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InfoPersonalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
